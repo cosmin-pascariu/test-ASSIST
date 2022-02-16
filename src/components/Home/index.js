1d1952e503ee63b3d8941188a24a954640097818
cosmin-pascariu <cosmin.pascariu2@student.usv.ro>
2022-02-16 17:07:21 +0200

    Update

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import {
   HomeContainer,
   HomeContent,
@@ -11,6 +11,40 @@ import {
 } from "./HomeElements";
 
 const Home = () => {
+  // const [cards, setCards] = useState([]);
+  //   useEffect(() => {
+  //
+  //     async function fetchData() {
+  //       const fetchResponse = await fetch(
+  //         "https://techcrunch.com/wp-json/wp/v2/posts?per_page=100&context-embed"
+  //       );
+  //       const data = await fetchResponse.json();
+  //       if (!data) {
+  //         console.log("Something went wrong");
+  //         return;
+  //       }
+
+  //       const infoArray = data;
+
+  //       const newCards = infoArray.map((card, i) => <HomeCard>
+  //   <CardTitle>Title</CardTitle>
+  //   <CardImage></CardImage>
+  //   <CardDescription>
+  //     <FirstLine>
+  //       <span>DESCRIPTION</span>
+  //       <span>Date</span>
+  //     </FirstLine>
+  //     <SecondLine>
+  //       <button>Edit</button>
+  //       <button>Delete</button>
+  //     </SecondLine>
+  //   </CardDescription>
+  // </HomeCard>);
+  //       setCards(newCards);
+  //     }
+  //     fetchData();
+  //   }, []);
+
   return (
     <HomeContainer>
       <HomeContent>
